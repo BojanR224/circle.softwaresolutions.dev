45e40fb48af2ec32e59b4ac7bf0569c1d9322b2c
Бојан Робев <61628838+BojanR224@users.noreply.github.com>
2023-12-01 15:58:30 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import { App } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/circle.softwaresolutions.dev">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
